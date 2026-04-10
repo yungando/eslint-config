62f45d777387a3103a5fe239df17eea6f96be05c
James Anderson <yung@ando.ws>
2026-04-10 14:51:15 +0100

    Disable consistent chaining

--- src/configs/antfu.js
+++ src/configs/antfu.js
@@ -3,6 +3,7 @@ export default () => [
     name: 'yungando/antfu',
     /// keep-sorted
     rules: {
+      'antfu/consistent-chaining': 'off',
       'antfu/consistent-list-newline': 'off',
       'antfu/curly': 'off',
       'antfu/if-newline': 'off',
